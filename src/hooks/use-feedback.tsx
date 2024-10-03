d566372b740fd6158b824f73ad879687115452a0
aeither <36173828+aeither@users.noreply.github.com>
2024-10-03 22:48:22 +0200

    add more hooks

--- src/hooks/use-feedback.tsx
+++ src/hooks/use-feedback.tsx
@@ -15,7 +15,6 @@ export const useSubmitFeedback = () => {
   const baseUrl = chain?.blockExplorers?.default.url;
 
   const submitFeedback = async (feedback: string) => {
-    console.log("🚀 ~ useSubmitFeedback ~ baseUrl:", baseUrl);
     try {
       const txHash = await writeContractAsync({
         address: feedbackContractAddresses[chainId],
@@ -59,4 +58,107 @@ export const useSubmitFeedback = () => {
   };
 
   return { submitFeedback, hash, isPendingTx };
+};
+
+export const useAddWisdom = () => {
+  const {
+    writeContractAsync,
+    data: hash,
+    isPending: isPendingTx,
+  } = useWriteContract();
+  const chainId = useChainId();
+  const { chain } = useAccount();
+  const baseUrl = chain?.blockExplorers?.default.url;
+
+  const addWisdom = async (quote: string) => {
+    try {
+      const txHash = await writeContractAsync({
+        address: feedbackContractAddresses[chainId],
+        abi: FEEDBACK_CONTRACT_ABI,
+        functionName: "addWisdom",
+        args: [quote],
+      });
+
+      const publicClient = getWagmiPublicClient(chainId);
+      const receipt = await publicClient?.waitForTransactionReceipt({
+        hash: txHash,
+      });
+
+      if (receipt) {
+        toast({
+          title: "Success",
+          description: "Your wisdom has been submitted successfully.",
+          action: (
+            <ToastAction
+              onClick={() =>
+                window.open(
+                  `${baseUrl}/tx/${receipt.transactionHash}`,
+                  "_blank"
+                )
+              }
+              altText={"View Transaction"}
+            >
+              View Transaction
+            </ToastAction>
+          ),
+        });
+      }
+    } catch (error) {
+      console.error("Error submitting wisdom:", error);
+      toast({
+        title: "Error",
+        description: "Failed to submit wisdom. Please try again.",
+        variant: "destructive",
+      });
+    }
+  };
+
+  return { addWisdom, hash, isPendingTx };
+};
+export const useCanAddWisdom = () => {
+  const chainId = useChainId();
+  const { address } = useAccount();
+  const publicClient = getWagmiPublicClient(chainId);
+
+  const canAddWisdom = async () => {
+    if (!publicClient) {
+      throw new Error("Public client is not available");
+    }
+    const response = await publicClient.readContract({
+      address: feedbackContractAddresses[chainId],
+      abi: FEEDBACK_CONTRACT_ABI,
+      functionName: "canAddWisdom",
+      args: [],
+    });
+    return response;
+  };
+
+  const timeUntilNextWisdom = async () => {
+    if (!publicClient) {
+      throw new Error("Public client is not available");
+    }
+    return await publicClient.readContract({
+      address: feedbackContractAddresses[chainId],
+      abi: FEEDBACK_CONTRACT_ABI,
+      functionName: "timeUntilNextWisdom",
+      args: [],
+    });
+  };
+
+  const getLatestWisdom = async () => {
+    if (!publicClient) {
+      throw new Error("Public client is not available");
+    }
+    if (!address) {
+      throw new Error("Address is not available");
+    }
+    return await publicClient.readContract({
+      address: feedbackContractAddresses[chainId],
+      abi: FEEDBACK_CONTRACT_ABI,
+      functionName: "wisdomByUser",
+      args: [address],
+    });
+  };
+
+  return { canAddWisdom, timeUntilNextWisdom, getLatestWisdom };
 };
