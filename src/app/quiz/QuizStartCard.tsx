a0dfefab278a9ddb71f925861ef0c9338a237372
aeither <36173828+aeither@users.noreply.github.com>
2024-10-07 15:58:19 +0200

    copy clipboard

--- src/app/quiz/QuizStartCard.tsx
+++ src/app/quiz/QuizStartCard.tsx
@@ -8,7 +8,7 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { useQuizStore } from "@/store/quizStore";
-import { Share2 } from "lucide-react";
+import { Check, ClipboardCopy, Share2 } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -22,10 +22,10 @@ export function QuizStartCard() {
     quizId,
   } = useQuizStore();
   const [isShared, setIsShared] = useState(false);
+  const [isCopied, setIsCopied] = useState(false);
+  const shareUrl = `${window.location.origin}/quiz?id=${quizId}`;
 
   const handleShare = async () => {
-    const shareUrl = `${window.location.origin}/quiz?id=${quizId}`;
-
     if (navigator.share) {
       try {
         await navigator.share({
@@ -34,46 +34,78 @@ export function QuizStartCard() {
           url: shareUrl,
         });
         setIsShared(true);
+        setTimeout(() => setIsShared(false), 2000);
       } catch (error) {
         console.error("Error sharing:", error);
       }
     } else {
-      // Fallback for browsers that don't support Web Share API
-      navigator.clipboard.writeText(shareUrl);
-      setIsShared(true);
-      setTimeout(() => setIsShared(false), 2000);
+      handleCopy();
     }
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(shareUrl);
+    setIsCopied(true);
+    setTimeout(() => setIsCopied(false), 2000);
   };
 
   return (
     <Card className="flex flex-col w-full max-w-2xl mx-auto my-4">
-      <CardHeader className="space-y-2">
-        <div className="flex flex-wrap justify-between items-center gap-2">
+      <CardHeader className="space-y-4">
+        <div className="flex flex-wrap justify-between items-center gap-4">
           {quizCategory && (
             <Badge variant="secondary" className="text-xs">
               {quizCategory}
             </Badge>
           )}
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button variant="outline" size="icon" onClick={handleShare}>
-                  <Share2 className="h-4 w-4" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent>
-                {isShared ? "Shared!" : "Share Quiz"}
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
+          <div className="flex space-x-2">
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    onClick={handleCopy}
+                    className="relative"
+                  >
+                    {isCopied ? (
+                      <Check className="h-4 w-4 text-green-500" />
+                    ) : (
+                      <ClipboardCopy className="h-4 w-4" />
+                    )}
+                    {isCopied && (
+                      <span className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-black text-white text-xs py-1 px-2 rounded">
+                        Copied!
+                      </span>
+                    )}
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  {isCopied ? "Copied!" : "Copy Link"}
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button variant="outline" size="icon" onClick={handleShare}>
+                    <Share2 className="h-4 w-4" />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  {isShared ? "Shared!" : "Share Quiz"}
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </div>
         </div>
         <CardTitle className="text-xl sm:text-2xl md:text-3xl">
           {quizName}
         </CardTitle>
       </CardHeader>
-      <CardContent className="flex flex-grow flex-col h-auto justify-between p-4 sm:p-6">
+      <CardContent className="flex flex-grow flex-col h-auto justify-between p-6">
         {quizImage && (
-          <div className="mb-4">
+          <div className="mb-6">
             <Image
               src={quizImage}
               alt={quizName}
@@ -84,7 +116,7 @@ export function QuizStartCard() {
             />
           </div>
         )}
-        <p className="mb-6 text-sm sm:text-base">{quizDescription}</p>
+        <p className="mb-8 text-sm sm:text-base">{quizDescription}</p>
         <Button onClick={startQuiz} className="w-full text-lg py-6">
           Start Quiz
         </Button>
